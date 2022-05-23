33a769d32b8ba617f29402265debcdf09a96f097
Daniel Campos <danielcampos14@hotmail.com.br>
2022-05-23 12:26:59 -0300

    Tratando Regions

--- cleanDataset.py
+++ cleanDataset.py
@@ -1,3 +1,28 @@
 from Config import Config
+import pandas as pd
 
-Config.CLEANED_DATASET_DIR_PATH.mkdir(parents=True, exist_ok=True)
+
+def createRegionForNanRegion(df):
+    if df['NOC'] == 'ROT':
+        df['region'] = 'Refugee'
+    elif df['NOC'] == 'TUV':
+        df['region'] = 'Tuvalu'
+    elif df['NOC'] == 'UNK':
+        df['region'] = 'Unknown'
+    
+    return df
+
+def treatRegions():
+    regionsDf = pd.read_csv(Config.REGIONS_FILE_PATH)
+    regionsDf = regionsDf.apply(createRegionForNanRegion, axis=1)
+
+    regionsDf.to_csv(Config.CLEANED_REGIONS_FILE_PATH)
+    
+
+def treatAthletes():
+    athletesDf = pd.read_csv(Config.ATHLETES_FILE_PATH)
+
+if __name__ == "__main__":
+    Config.CLEANED_DATASET_DIR_PATH.mkdir(parents=True, exist_ok=True)
+    treatRegions()
+    #treatAthletes()
