0c9959ee2f26851184046dcd4c01ebb548981bae
Daniel Campos <danielcampos14@hotmail.com.br>
2022-05-24 15:42:33 -0300

    Adicionando tratamento athletes

--- cleanDataset.py
+++ cleanDataset.py
@@ -16,15 +16,16 @@ def treatRegions():
     regionsDf = pd.read_csv('./'+Config.ORIGINAL_DATASET_DIR_NAME+"/"+Config.REGIONS_FILE_NAME)
     regionsDf = regionsDf.apply(createRegionForNanRegion, axis=1)
 
-    regionsDf.to_csv('./'+Config.CLEANED_DATASET_DIR_NAME+"/"+Config.CLEANED_REGIONS_FILE_NAME)
+    regionsDf.to_csv('./'+Config.CLEANED_DATASET_DIR_NAME+"/"+Config.CLEANED_REGIONS_FILE_NAME, index=False)
     
 
 def treatAthletes():
     athletesDf = pd.read_csv('./'+Config.ORIGINAL_DATASET_DIR_NAME+"/"+Config.ATHLETES_FILE_NAME)
+    athletesDf['Medal'].fillna('None',inplace=True)
     #.
     #.
     #.
-    athletesDf.to_csv('./'+Config.CLEANED_DATASET_DIR_NAME+"/"+Config.CLEANED_ATHLETES_FILE_NAME)
+    athletesDf.to_csv('./'+Config.CLEANED_DATASET_DIR_NAME+"/"+Config.CLEANED_ATHLETES_FILE_NAME, index=False)
 
 if __name__ == "__main__":
     #Config.CLEANED_DATASET_DIR_PATH.mkdir(parents=True, exist_ok=True)
